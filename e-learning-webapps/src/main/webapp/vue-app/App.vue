f774ced57d01cbc0271cc5c251d1579ff0847f8f
Intern Yen <yendp27@gmail.com>
2021-01-06 17:17:06 +0700

    learning

---
+++ e-learning-webapps/src/main/webapp/vue-app/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <app-nav-main></app-nav-main>
+        <router-view />
+    </div>
+</template>
+
+<script>
+
+    import AppNavMain from './components/commun/AppNavMain.vue'
+
+    export default {
+        components:{AppNavMain},
+    }
+</script>
+
+
